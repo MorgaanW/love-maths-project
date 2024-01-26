8b9098d82fcf426096a6e68256d8ec35970fa0d5
Morgan W <morganwilliams632@gmail.com>
2024-01-26 13:57:26 +0000

    added structure to correct answer func

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,8 +37,23 @@ function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer() {
+/**
+ * gets the numbers (operands) and the operator
+ */
 
+
+
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById(`operand1`).innerText)
+    let operand2 = parseInt(document.getElementById(`operand2`).innerText)
+    let operator = document.getElementById("operator").innerText;
+
+    if(operator === "+") {
+        return [operand1 + operand2, "addition"]
+    } else {
+        alert(`Unimplemented operator ${operator}`)
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
